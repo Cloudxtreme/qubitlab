d7288e7e31154661fc4f27b924d327f9bfdbae40
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-11-18 16:11:34 +0100

    Basic diagram.

---
+++ core/diagram.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import re
+
+
+class Diagram:
+    def __init__(self):
+        self.colors = {
+            'red': '\033[31m',
+            'green': '\033[32m',
+            'yellow': '\033[33m',
+            'blue': '\033[34m',
+            'violet': '\033[35m',
+            'cyan': '\033[36m',
+            'gray': '\033[90m',
+            'reset': '\033[0m'
+        }
+
+    @staticmethod
+    def test_colors():
+        for x in range(0, 255):
+            print '\033[' + str(x) + 'm' + "CODE: " + str(x) + '\033[0m',
+
+    def display(self, file_path, active_step=None):
+        steps_line = self.get_steps_line(file_path)
+        step_pos = self.get_step_pos(active_step, steps_line)
+
+        active_color = self.colors['yellow']
+        inactive_color = self.colors['reset']
+        reset_color = self.colors['reset']
+        no_circuit_color = self.colors['gray']
+
+        print no_circuit_color + "Step " + str(active_step) + reset_color
+
+        with open(file_path) as f:
+            if active_step is None:
+                for line in f:
+                    if self.check_if_is_no_circuit_line(line):
+                        print no_circuit_color + line.rstrip() + reset_color
+                    else:
+                        print active_color
+                        print line.rstrip()
+                        print reset_color
+            else:
+                for line in f:
+                    if self.check_if_is_no_circuit_line(line):
+                        print no_circuit_color + line.rstrip() + reset_color
+                    else:
+                        print "{0}{1}{2}{3}{4}{5}".format(active_color, line[:step_pos], reset_color, inactive_color,
+                                                          line[step_pos:].rstrip(), reset_color)
+
+    def get_steps_line(self, file_path):
+        with open(file_path) as f:
+            for line in f:
+                if self.check_if_is_steps_line(line):
+                    return line.rstrip()
+        return ''
+
+    def check_if_is_no_circuit_line(self, line):
+        if self.check_if_is_steps_line(line):
+            return True
+        if self.check_if_is_dots_line(line):
+            return True
+        return False
+
+    @staticmethod
+    def check_if_is_steps_line(line):
+        steps_line_pattern = re.compile('\s*(\|\d+\s*)+\s*')
+        line_match = steps_line_pattern.match(line)
+        if line_match:
+            return True
+        return False
+
+    @staticmethod
+    def check_if_is_dots_line(line):
+        steps_line_pattern = re.compile('\s*\.+\s*')
+        line_match = steps_line_pattern.match(line)
+        if line_match:
+            return True
+        return False
+
+    @staticmethod
+    def get_step_pos(step, steps_line):
+        return re.search(("\|" + str(step) + "\s*(\||$)"), steps_line).start()
