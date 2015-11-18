5f15c286a841daafb3b1a4bd7e7eebebd5687659
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-11-18 21:46:09 +0100

    Import circuits modules - update 2.

--- data/circuits/teleportation/module.py
+++ data/circuits/teleportation/module.py
@@ -4,8 +4,12 @@ def get_title():
 
 
 def get_quantum_state_for_step(step):
+    if None is step:
+        return 'quantum state for final step...'
     return 'quantum state for step ' + str(step) + '...'
 
 
 def get_measure_results_for_step(step):
+    if None is step:
+        return 'measure results for final step...'
     return 'measure results for step ' + str(step) + '...'
